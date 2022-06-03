5f52f77c606587f5db69353637908644087b70d4
John Kinyua <kingjohnkn@gmail.com>
2022-06-03 07:04:43 -0700

    lower upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase letters */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		/* print UPPERCASE letters */
+		for (ch = 'A'; ch <= 'Z'; ch++)
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
