ea20bc97448e9750c8dcb1229433cf7058d8b484
John Kinyua <kingjohnkn@gmail.com>
2022-06-03 07:53:27 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 0 - 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
