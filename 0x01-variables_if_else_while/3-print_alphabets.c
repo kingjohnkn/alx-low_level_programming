36c226ff6d81691328971d31f8db5b1322ca38c2
John Kinyua <kingjohnkn@gmail.com>
2022-06-03 07:10:26 -0700

    low-uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,12 +13,12 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
-		/* print UPPERCASE letters */
-		for (ch = 'A'; ch <= 'Z'; ch++)
-		{
-			putchar(ch);
-		}
 	}
+	/* print UPPERCASE letters */
+        for (ch = 'A'; ch <= 'Z'; ch++)
+        {
+		putchar(ch);
+        }
 	putchar('\n');
 	return (0);
 }
