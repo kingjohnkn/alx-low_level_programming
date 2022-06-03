0140723c09f10d3cd7c4efb00d1b133373692eb1
John Kinyua <kingjohnkn@gmail.com>
2022-06-03 06:28:39 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase letters */
+	for (ch='a'; ch<='z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
