ed35ed4a526aaa837fe9450a1d773c29b712da3e
John Kinyua <kingjohnkn@gmail.com>
2022-06-03 08:03:13 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
