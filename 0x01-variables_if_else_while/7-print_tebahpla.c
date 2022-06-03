99f35435e4193a4bb7c705db12c284aad4d73ed1
John Kinyua <kingjohnkn@gmail.com>
2022-06-03 09:50:40 -0700

    reverse alph

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase letters */
+	for (ch = 'z'; ch >= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
