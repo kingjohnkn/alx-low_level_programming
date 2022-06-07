36655758c5851fce8fbe945b83aa94ff7e60cb7d
John Kinyua <kingjohnkn@gmail.com>
2022-06-07 14:00:59 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - print the alphabet in lowercase
+ * print_alphabet - print the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int i;
-	
+
 	i = 97;
 	while (i <= 122)
 	{
