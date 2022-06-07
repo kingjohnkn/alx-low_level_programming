dadb822b1d8eefe2faf8bf5659988bbd36f2fa8b
John Kinyua <kingjohnkn@gmail.com>
2022-06-07 13:30:43 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    i = 97;
+    while (i <= 122)
+    {
+	    _putchar(i);
+	    i++;
+    }
+    _putchar('\n');
+    return (0);
+}
