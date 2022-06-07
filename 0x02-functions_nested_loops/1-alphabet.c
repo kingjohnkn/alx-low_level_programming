13d7a52fd07930b79bda385d2870b63ff328fee1
John Kinyua <kingjohnkn@gmail.com>
2022-06-07 13:55:28 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-    int i;
-
-    i = 97;
-    while (i <= 122)
-    {
-	    _putchar(i);
-	    i++;
-    }
-    _putchar('\n');
-    return (0);
+	int i;
+	
+	i = 97;
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
 }
