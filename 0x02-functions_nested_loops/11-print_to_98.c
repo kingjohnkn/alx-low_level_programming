53f6cf5b2414d38ea6704ca7bbf323e55f776b9d
John Kinyua <kingjohnkn@gmail.com>
2022-06-07 17:32:09 -0700

    n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line.
+ *
+ * @n: initial number.
+ *
+ * Return: Add of number one and number two.
+ */
+void print_to_98(int n)
+{
+}
